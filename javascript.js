5e6e05e89f9ec39f2c7759c21e7c6d65a08aa75e
themagicianking <tmoonder@gmail.com>
2023-04-08 10:55:26 -0400

    Refactored numbered button code to make it more efficient.

--- javascript.js
+++ javascript.js
@@ -40,72 +40,14 @@ function addToDisplay (x) {
     display.textContent = x;
 }
 
-const one = document.getElementById("one");
-one.addEventListener("click", addOne);
-function addOne() {
-    displayNum = displayNum + "1";
-    addToDisplay(displayNum);
+let numbers = document.querySelectorAll("button.number");
+
+for (let i = 0; i < 10; i++) {
+    numbers[i].addEventListener("click", storeDisplay);
+    function storeDisplay () {
+        displayNum = displayNum + numbers[i].textContent;
+        addToDisplay(displayNum);
+    };
 };
 
-const two = document.getElementById("two");
-two.addEventListener("click", addTwo);
-function addTwo() {
-    displayNum = displayNum + "2";
-    addToDisplay(displayNum);
-};
-
-const three = document.getElementById("three");
-three.addEventListener("click", addThree);
-function addThree() {
-    displayNum = displayNum + "3";
-    addToDisplay(displayNum);
-};
-
-const four = document.getElementById("four");
-four.addEventListener("click", addFour);
-function addFour() {
-    displayNum = displayNum + "4";
-    addToDisplay(displayNum);
-};
-
-const five = document.getElementById("five");
-five.addEventListener("click", addFive);
-function addFive() {
-    displayNum = displayNum + "5";
-    addToDisplay(displayNum);
-};
-
-const six = document.getElementById("six");
-six.addEventListener("click", addSix);
-function addSix() {
-    displayNum = displayNum + "6";
-    addToDisplay(displayNum);
-};
-
-const seven = document.getElementById("seven");
-seven.addEventListener("click", addSeven);
-function addSeven() {
-    displayNum = displayNum + "7";
-    addToDisplay(displayNum);
-};
-
-const eight = document.getElementById("eight");
-eight.addEventListener("click", addEight);
-function addEight() {
-    displayNum = displayNum + "8";
-    addToDisplay(displayNum);
-};
-
-const nine = document.getElementById("nine");
-nine.addEventListener("click", addNine);
-function addNine() {
-    displayNum = displayNum + "9";
-    addToDisplay(displayNum);
-};
-
-const zero = document.getElementById("zero");
-zero.addEventListener("click", addZero);
-function addZero() {
-    displayNum = displayNum + "0";
-    addToDisplay(displayNum);
-};
+//const clear = document.getElementById("")
